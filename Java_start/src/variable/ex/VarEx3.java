f2d65ad13a7db0b04409309772999282858c4b77
chanhyeok3856 <grass3856@gmail.com>
2024-06-29 19:30:45 +0900

    0629 java practice

---
+++ Java_start/src/variable/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long backup = 100000000L;// L 끝에 써주는 것 잊지 말기!!
+        boolean tro = true;
+        System.out.println(backup);
+        System.out.println(tro);
+    }
+}
