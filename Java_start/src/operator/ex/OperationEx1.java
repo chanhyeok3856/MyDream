217f9189f595732e4a6e68bdf72a96799b13626f
chanhyeok3856 <grass3856@gmail.com>
2024-07-01 18:18:42 +0900

    0701

---
+++ Java_start/src/operator/ex/OperationEx1.java
@@ -0,0 +1,13 @@
+package operator.ex;
+
+public class OperationEx1 {
+    public static void main(String[] args) {
+        int num1  = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int sum = num1 + num2 + num3;
+        int average = sum / 3; // 자바에서 int 끼리의 나눗셈은 자동으로 소수점 이하를 버린다.
+        System.out.println("average = " + average);
+        System.out.println("sum = " + sum);
+    }
+}
--- Java_start/src/variable/ex/VarEx3.java
+++ Java_start/src/variable/ex/VarEx3.java
@@ -2,7 +2,7 @@ package variable.ex;
 
 public class VarEx3 {
     public static void main(String[] args) {
-        long backup = 100000000L;// L 끝에 써주는 것 잊지 말기!!
+        long backup = 100000000L;// 리터럴은 기본 , L 끝에 써주는 것 잊지 말기!!
         boolean tro = true;
         System.out.println(backup);
         System.out.println(tro);
